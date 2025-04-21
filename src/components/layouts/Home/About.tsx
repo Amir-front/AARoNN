699636fcaf1742951d329bb12c1f76f29c1c458c
amir <amir@gmail.com>
2025-04-21 20:34:05 +0330

    last edit

--- src/components/layouts/Home/About.tsx
+++ src/components/layouts/Home/About.tsx
@@ -3,7 +3,7 @@ import Pic from "../../../assets/Home/1.png";
 
 const About = () => {
   return (
-    <section className="flex flex-col md:flex-row justify-center items-center p-5 text-white">
+    <section className="flex flex-col-reverse md:flex-row justify-center items-center p-5 text-white">
       <div className="w-full md:w-1/2 md:m-10 mb-5">
         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Let’s get know about me closer</h1>
         <p className="text-gray-400 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-6">
@@ -15,7 +15,7 @@ const About = () => {
           <button className="border-2 border-orange-400 bg-orange-400 flex flex-row p-5 rounded-4xl justify-center items-center mt-10 hover:py-6 cursor-pointer active:py-6 transition-all text-lg md:text-xl lg:text-2xl">Discover More About Me</button>
         </Link>
       </div>
-      <img src={Pic} alt="Pic" className="mt-12" />
+      <img src={Pic} alt="Pic" className="mb-12" />
     </section>
   );
 };
